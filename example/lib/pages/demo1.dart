054b4f036edf7fcbf7f138ca5f3615f16c08a1ae
zzzgydi <zzzgydi@gmail.com>
2024-01-07 17:09:08 +0800

    chore: add constant variable demo

--- example/lib/pages/demo1.dart
+++ example/lib/pages/demo1.dart
@@ -5,21 +5,41 @@ import 'div.dart';
 class Demo1Page extends StatelessWidget {
   const Demo1Page({super.key});
 
+  final String test = "";
+
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    const className =
+        "demo1 items-center test-SimpleIdentifier & inConstantContext";
+
+    const a = "this is abc";
+    const className2 = "demo1 test $a";
+
+    return Column(
       children: [
-        Text('Hello'),
+        const Text('Hello'),
         Div(
           className: "flex items-center demo1",
           children: [
             Div(
               className: "text demo1",
               children: [
-                Text("test"),
-                Div(
+                const Text("test"),
+                const Div(
                   className: "demo1 flex inline items-center",
                   children: [Text("test")],
+                ),
+                const Div(
+                  className: className,
+                  children: [Text("test")],
+                ),
+                const Div(
+                  className: className2,
+                  children: [Text("test")],
+                ),
+                Div(
+                  className: test,
+                  children: const [Text("test")],
                 ),
               ],
             )
