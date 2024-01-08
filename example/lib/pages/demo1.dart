6802fd5ed06fefb613e1f889779d6d40530fb2f1
GyDi <zzzgydi@gmail.com>
2024-01-08 17:01:13 +0800

    fix: adjust demo

--- example/lib/pages/demo1.dart
+++ example/lib/pages/demo1.dart
@@ -9,42 +9,37 @@ class Demo1Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const className =
-        "demo1 items-center test-SimpleIdentifier & inConstantContext";
+    const className = "items-center bg-black";
 
-    const a = "this is abc";
-    const className2 = "demo1 test $a";
-
-    return Column(
+    return const Div(
+      className: className,
       children: [
-        const Text('Hello'),
         Div(
-          className: "flex items-center demo1",
+          className: "bg-red-500 pt-[10] px-[100] py-[20]",
           children: [
             Div(
-              className: "p-10 bg-red-500 pt-[100]",
+              className: "p-10 bg-gray-400",
               children: [
-                const Text("test"),
-                const Div(
-                  className: "flex inline items-center border-2 border-red-500",
-                  children: [Text("test")],
-                ),
-                const Div(
-                  className: className,
-                  children: [Text("test")],
-                ),
-                const Div(
-                  className: className2,
-                  children: [Text("test")],
-                ),
-                Div(
-                  className: test,
-                  children: const [Text("test")],
-                ),
+                Text('This is'),
+                Text('EasyDSL'),
               ],
-            )
+            ),
+            SizedBox(height: 20),
+            Div(
+              className: "flex items-center p-[10] bg-gray-500",
+              children: [
+                Text('Hello'),
+                SizedBox(width: 10),
+                Div(
+                  className: "bg-orange-100 p-2",
+                  children: [
+                    Text('World'),
+                  ],
+                )
+              ],
+            ),
           ],
-        )
+        ),
       ],
     );
   }
